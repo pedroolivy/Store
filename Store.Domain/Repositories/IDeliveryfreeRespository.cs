7f07db547cf72b4672bd7bd0ff16f7fd043101e4
Pedro <pedrooliveiradevs@gmail.com>
2025-04-26 12:06:02 -0300

    Adicionando abstração dos repositorios

---
+++ Store.Domain/Repositories/IDeliveryfreeRespository.cs
@@ -0,0 +1,7 @@
+﻿namespace Store.Domain.Repositories
+{
+    public interface IDeliveryfreeRespository
+    {
+        decimal Get(string zipCode);
+    }
+}
---
+++ Store.Domain/Repositories/IOrderRepositoty.cs
@@ -0,0 +1,9 @@
+﻿using Store.Domain.Entities;
+
+namespace Store.Domain.Repositories
+{
+    public interface IOrderRepositoty
+    {
+        void Save(Order order);
+    }
+}
